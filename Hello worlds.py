3f69a15e4ac86fcc5c5c593c1381c703001d1dbb
johnnyheineken <janhynek@gmail.com>
2017-07-08 12:32:13 +0200

    very important function
    yes, indeed.

--- Hello worlds.py
+++ Hello worlds.py
@@ -5,3 +5,8 @@
 
 print('Hello darkness, my old friend')
 print('I ve come to talk with you again')
+def fun_ktera_dela_lidi_awesome(name)
+    print('who is awesome?')
+    print(name + ' is awesome!')
+    
+fun_ktera_dela_lidi_awesome('Honza')
