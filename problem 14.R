d39b71651a933558b1966ada7d1d82f2f6fbc51e
kasrad <kasrad@users.noreply.github.com>
2017-07-19 16:50:39 +0200

    p 14
    already under 60 seconds, yet still inefficient as fuck

--- problem 14.R
+++ problem 14.R
@@ -18,12 +18,11 @@ collatz<- function (x) {
 #je treba vymyslet, ktery cisla muzes rovnou poslat do prdele - napr ty pod 500K budou mit dycky o jeden krok
 #min, nez jejich dvojnasobky
 results<-matrix(nrow=1000000,ncol=1)
-profvis{
-  for (j in 500000:1000000){
+profvis(
+  for (j in setdiff((500001:1000000),3*seq(166666,333333,2)+1)){
     i<-1
     results[j,1]<-collatz(j)
-    print(j)
   }
-}
+)
 
-which.max(results)
+print(paste('the answer is',which.max(results)))
