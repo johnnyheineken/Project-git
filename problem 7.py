f60693d74eeb41ca9882e2c011c3d04fcd054053
kasrad <kasrad@users.noreply.github.com>
2017-07-27 17:56:32 +0200

    fix to p7
    this is a fix of minor bug in the code for problem 7

--- problem 7.py
+++ problem 7.py
@@ -15,13 +15,13 @@ import time
 start = time.time()
 hodne=150000
 potential_primes=np.arange(5,hodne, step=2)
-primes=np.array([2,3,5])
+primes=np.array([2,3])
 
 
 for i in potential_primes:
         if( 0 not in (i % primes[math.sqrt(i)>=primes])):
             primes=np.append(primes,i)          
-        if len(primes)==10002:
+        if len(primes)==10001:
             break
             
 print('the number is ' + str(primes[len(primes)-1]))
